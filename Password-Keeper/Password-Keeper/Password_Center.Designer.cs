1ef03f0f5b0ad1eaa25b3e5f61c0c194685a7de3
TheRandoman123 <nerfsteven@gmail.com>
2017-10-11 10:27:39 -0400

    Merge Setup

--- Password-Keeper/Password-Keeper/Password_Center.Designer.cs
+++ Password-Keeper/Password-Keeper/Password_Center.Designer.cs
@@ -28,19 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Password_Center));
-            this.SuspendLayout();
-            // 
-            // Password_Center
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1250, 585);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "Password_Center";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Password_Center";
-            this.ResumeLayout(false);
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Password_Center));
+			this.SuspendLayout();
+			// 
+			// Password_Center
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1250, 585);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "Password_Center";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Password_Center";
+			this.ResumeLayout(false);
 
         }
 
